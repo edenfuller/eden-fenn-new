57d7a92ad34f7bbf51a117930a5e4d9eb919aa03
edenfuller <equinoxfires@gmail.com>
2025-05-25 13:12:53 -0400

    Various updates

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,5 +15,6 @@ export default defineConfig({
         }
       },
     },
+    // mkcert(),
   ],
 });
